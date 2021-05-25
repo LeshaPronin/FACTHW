0b78556b2789b9ca08875e7ec26c19c307a618fc
Pronin Aleksey <geekan56@gmail.com>
2021-05-25 20:48:19 +0500

    Lesson assignments 12.05.21

--- index.php
+++ index.php
@@ -6,101 +6,93 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>HomeWork Pronin</title>
+    <title>HomeWork Pronin 10.05.</title>
 </head>
 <body>
+<h1>Задания</h1>
 <div class="container">
+
     <span>Задание 1</span>
+    <form action="#" method="post">
+        <label for="checkbox">Чекбокс 1</label>
+        <input type="checkbox" name="checkbox"><br>
 
+        <label for="checkbox">Чекбокс 2</label>
+        <input type="checkbox" name="checkbox"><br>
+
+        <label for="checkbox">Чекбокс 3</label>
+        <input type="checkbox" name="checkbox"><br>
+
+
+        <label for="radio">Радио 1</label>
+        <input type="radio" name="radio"><br>
+
+        <label for="radio">Радио 2</label>
+        <input type="radio" name="radio"><br>
+
+        <label for="radio">Радио 3</label>
+        <input type="radio" name="radio"><br>
+
+
+        <input type="text" name="text"><br>
+
+        <textarea name="textarea" id="" cols="22" rows="9"></textarea><br>
+        <input type="submit">
+    </form>
     <?php
-        function fun1($arr1) {
-            foreach ($arr1 as $key1 => $value1) {
-                $arr1[$key1] = rand(1, 100);
-                echo $arr1[$key1]."<br>";
-            }
-
-            return ($arr1);
-        }
-
-        $arr1 = array("1", "2", "3", "4", "5");
-        fun1($arr1);
+    echo "Чекбокс: ".$_POST['checkbox']."<br>";
+    echo "Радио: ".$_POST['radio']."<br>";
+    echo "Короткий текст: ".$_POST['text']."<br>";
+    echo "Длинный текст: ".$_POST['textarea']."<br>";
     ?>
 
     <span>Задание 2</span>
 
+    <form action="" method="post">
+        <label for="login">Логин:</label>
+        <select name="login">
+            <option value="v1">Админ</option>
+            <option value="v2">Не админ</option>
+            <option value="v3">Еще не админ</option>
+            <option value="v4">И ещё не админ</option>
+            <option value="v5">Ну и ещё не админ</option>
+        </select>
+        <label for="password">Пароль:</label>
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+    
     <?php
-        $stroke = "HTML, CSS, PHP, BITRIX";
 
-        function fun2($stroke) {
-            $str1 = explode (', ', $stroke);
-            $count = 0;
-            foreach ($str1 as $key2=>$value2) {
-                $count++;
-            }
-            echo "Количество слов в строке = ".$count;
-        }
-        fun2($stroke);
+    $login = $_POST['login'];
+    $pass = $_POST['password'];
+
+    if ($login=="v1" && $pass=="123321qwe") {
+        echo "<script>self.location='/admin.php';</script>";
+    }
+    else {
+        echo "Неверный логин или пароль";
+    }
     ?>
 
     <span>Задание 3</span>
 
-    <?php
-        $stroke2 = "HTML, CSS, PHP, BITRIX";
-
-        function fun3($stroke2) {
-            $str2 = explode(', ', $stroke2);
-            for ($i=0; $i<5; $i++) {
-                $bukv[$i] = str_split($str2[$i]);
-            }
-            $c=0;
-            for ($u=3; $u>=0; $u=$u-1) {
-                foreach ($bukv[$u] as $key4=>$value4) {
-                    $c++;
-                }
-                for ($c; $c>=0; $c=$c-1) {
-                    echo $bukv[$u][$c];
-                }
-            }
-        }
-
-        fun3($stroke2);
-
-    ?>
-
-    <span>Задание 4</span>
+    <form action="" method="post">
+        <select name="link" id="">
+            <option value="1">Лаб 1</option>
+            <option value="2">Лаб 2</option>
+            <option value="3">Лаб 3</option>
+            <option value="4">Лаб 4</option>
+        </select>
+        <input type="submit">
+    </form>
 
     <?php
-        $stroke4 = "HTML, CSS, PHP, BITRIX";
+        $nomber=$_POST['link'];
 
-        function fun4($stroke4) {
-            $count4 = strlen($stroke4);
-            echo $count4;
-
-        }
-
-        fun4($stroke4);
+        echo "Ссылка на лабораторную - ";
     ?>
-
-    <span>Задание 5</span>
-
-    <?php
-    $stroke5 = "HTML, CSS, PHP, BITRIX";
-
-    function fun5($stroke5) {
-        $str5 = explode(', ', $stroke5);
-        for ($x=0; $x<4; $x++) {
-            $bukv5[$x] = str_split($str5[$x]);
-        }
-
-        for ($u=3; $u>=0; $u=$u-1) {
-            for ($c=0; $c<7; $c++) {
-                echo $bukv5[$u][$c]."<br>";
-            }
-        }
-    }
-
-    fun5($stroke5)
-    ?>
+    <a href="<?echo "phpcourse.php?l=".$nomber;?>"><?echo "phpcourse.php?l=".$nomber;?></a>
 </body>
 </html>
 
